021198df080fd7c1b43539061d649c5974c914ee
csynoers <3s0c9m7@gmail.com>
2019-10-11 15:13:58 +0700

    add ss

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 <div class="container border">
     <hr>
     <h2>LIST OF SINUR LANDING PAGES</h2>
+    <h5>untuk melihat screenshoot posisikan mouse pada judul landing page</h5>
     <hr>
     <div class="container">
       <table class="table table-hover">
@@ -32,14 +33,14 @@
         <tbody>
           <?php
             foreach (array_filter(glob('*'), 'is_dir') as $key => $value) {
-              echo '
+              echo "
                 <tr>
-                  <td>'.$value.'</td>
+                  <td><label class='tr-content' data-name='{$value}'>{$value}</label></td>
                   <td>
-                    <a href="'.$value.'" class="text-white btn btn-primary">view demo</a>
+                    <a href='$value' class='text-white btn btn-primary'>view demo</a>
                   </td>
                 </tr>
-              ';
+              ";
             }
           ?>
         </tbody>
@@ -48,5 +49,46 @@
     <!-- /.container -->
 </div>
 
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+    
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+      
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<script>
+  $(document).ready(function(){
+    // $(".tr-content").on('mouseenter mouseleave',function(){
+    $(".tr-content").on('mouseenter',function(){
+      var modal = $("#myModal");
+      modal.find('.modal-title').text('SS '+$(this).data('name'));
+      modal.find('.modal-body').html('<img src="'+$(this).data('name')+'/ss.png" class="img-fluid" alt="'+$(this).data('name')+'">');
+      modal.modal("show");
+    })
+    // click(function(){
+    //   $("#myModal").modal("show");
+    // });
+    // $("#myModal").on('show.bs.modal', function () {
+    //   alert('The modal is about to be shown.');
+    // });
+  })
+</script>
 </body>
 </html>
